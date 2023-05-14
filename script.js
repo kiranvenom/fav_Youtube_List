1add4d8b83368dcb0f0151b76b2acae061065133
Kiran <km060360@gmail.com>
2023-05-14 23:16:31 +0530

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const form = document.getElementById('first-form');
 const input = document.getElementById('key-input');
 const ul = document.getElementById('item-list');
+const ClearButt = document.querySelector('.clear-button-all');
 
 
 const heading = document.querySelector('h1');
-heading.textContent = 'Favorite Youtube Channel';
+heading.innerText = 'Favorite Youtube Channel';
 
 //functions
 addItem = (e) => {
@@ -26,13 +27,44 @@ addItem = (e) => {
 
         li.textContent = input.value;
         li.className = 'items';
+        butt.className = 'remove-item'
         icon.className = 'ri-close-fill';
         li.appendChild(butt);
         butt.appendChild(icon);
         ul.appendChild(li);
         input.value = '';
     }
+    checkUi();
 };
+
+onClickRmItem = (e) => {
+    if(e.target.parentElement.classList.contains('remove-item')){
+        e.target.parentElement.parentElement.remove();
+    }
+    checkUi();
+};
+
+clearAllButton = (e) => {
+    const li = document.querySelectorAll('li');
+    li.forEach((elem)=>{
+        elem.remove();
+    });
+    checkUi();
+};
+
+checkUi = () => {
+    const li = ul.querySelectorAll('li');
+    const clearrr = document.querySelector('.clear');
+    if(li.length === 0){
+        clearrr.style.display = 'none';
+    }else {
+        clearrr.style.display = 'block';
+    }
+};
+checkUi();
+
 
 //event listeners
 form.addEventListener('submit', addItem);
+ul.addEventListener('click', onClickRmItem);
+ClearButt.addEventListener('click', clearAllButton);
